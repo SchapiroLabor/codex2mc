588f44c8884cabdd0ba848150677c1373f60a9d1
VictorDidier <vdpm85@hotmail.com>
2025-05-23 15:17:37 +0200

    blur_effect is now used as metric for best plane

--- codex2mc/best_plane.py
+++ codex2mc/best_plane.py
@@ -9,14 +9,15 @@ def extract(cycle_info,method="propagate"):
     then it selects the median plane index from all the tiles and it propagates this index across all other channels and tiles.
     per_channel: selects the plane with highest contrast for each channel and each tile.  Not recommended. 
     '''
+    metric="blur"#opts are blur and contrast median, blur works best when h_size=3
     if method=="propagate":
-        aux_df=cycle_info.loc[cycle_info["channel"]==1,["tile","plane","contrast_median"]]
-        focused_plane=aux_df.loc[aux_df.groupby(["tile"])["contrast_median"].idxmax()]["plane"].median()
+        aux_df=cycle_info.loc[cycle_info["channel"]==1,["tile","plane",metric]]
+        focused_plane=aux_df.loc[aux_df.groupby(["tile"])[metric].idxmax()]["plane"].median()
         cycle_info=cycle_info.loc[cycle_info["plane"]==int(focused_plane)]
 
     elif method=="per_channel":
 
-        cycle_info=cycle_info.loc[cycle_info.groupby(["channel", "tile"])["contrast_median"].idxmax()]
+        cycle_info=cycle_info.loc[cycle_info.groupby(["channel", "tile"])[metric].idxmax()]
 
     return cycle_info
 
